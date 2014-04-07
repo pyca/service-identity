be830a0607a96b757379bdbaf4baf3885025035c
Hynek Schlawack <schlawack@variomedia.de>
2014-04-08 01:31:02 +0200

    Start 0.3 cycle

--- service_identity/__init__.py
+++ service_identity/__init__.py
@@ -4,7 +4,7 @@ Verify service identities.
 
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.2"
+__version__ = "0.3.0"
 __author__ = "Hynek Schlawack"
 __license__ = "MIT"
 
