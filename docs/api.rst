979d0accf227fffbb1a5f34f355876b5066e8884
Hynek Schlawack <schlawack@variomedia.de>
2014-06-13 12:08:10 +0200

    Hide APIs that aren't really public yet
    Also remove corresponding warnings.

--- docs/api.rst
+++ docs/api.rst
@@ -2,13 +2,6 @@
 API
 ===
 
-.. warning::
-
-   There are public-looking APIs present in ``service_identity`` that aren't documented here.
-   Those are *not* public *yet*.
-   *Only* APIs documented here are final and production-ready.
-
-
 .. currentmodule:: service_identity.pyopenssl
 
 
--- service_identity/__init__.py
+++ service_identity/__init__.py
@@ -11,36 +11,11 @@ __license__ = "MIT"
 from . import pyopenssl
 from .exceptions import (
     CertificateError,
-    DNSMismatch,
-    SRVMismatch,
-    URIMismatch,
     VerificationError,
 )
-from ._common import (
-    DNSPattern,
-    DNS_ID,
-    SRVPattern,
-    SRV_ID,
-    ServiceMatch,
-    URIPattern,
-    URI_ID,
-    verify_service_identity,
-)
-
 
 __all__ = [
     "CertificateError",
-    "DNSMismatch",
-    "DNSPattern",
-    "DNS_ID",
-    "SRVMismatch",
-    "SRVPattern",
-    "SRV_ID",
-    "ServiceMatch",
-    "URIMismatch",
-    "URIPattern",
-    "URI_ID",
     "VerificationError",
     "pyopenssl",
-    "verify_service_identity",
 ]
