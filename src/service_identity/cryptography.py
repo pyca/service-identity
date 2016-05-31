80e12a1e19c9bb68c4d7114ce27e8632f055db2c
Tristan Seligmann <mithrandi@mithrandi.net>
2016-05-31 22:01:17 +0200

    Fix some stuff.

--- src/service_identity/cryptography.py
+++ src/service_identity/cryptography.py
@@ -4,8 +4,11 @@
 
 from __future__ import absolute_import, division, print_function
 
+import warnings
+
 from cryptography.x509 import (
-    DNSName, ExtensionOID, OtherName, UniformResourceIdentifier)
+    DNSName, ExtensionOID, NameOID, OtherName, UniformResourceIdentifier)
+from cryptography.x509.extensions import ExtensionNotFound
 from pyasn1.codec.der.decoder import decode
 from pyasn1.type.char import IA5String
 
@@ -62,20 +65,43 @@ def extract_ids(cert):
     :return: List of IDs.
     """
     ids = []
-    ext = cert.extensions.get_extension_for_oid(
+    try:
+        ext = cert.extensions.get_extension_for_oid(
             ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
-    ids.extend([DNSPattern(name.encode('utf-8'))
-                for name
-                in ext.value.get_values_for_type(DNSName)])
-    ids.extend([URIPattern(uri.encode('utf-8'))
-                for uri
-                in ext.value.get_values_for_type(UniformResourceIdentifier)])
-    for other in ext.value.get_values_for_type(OtherName):
-        if other.type_id == ID_ON_DNS_SRV:
-            srv, _ = decode(other.value)
-            if isinstance(srv, IA5String):
-                ids.append(SRVPattern(srv.asOctets()))
-            else:  # pragma: nocover
-                raise CertificateError(
-                    "Unexpected certificate content."
-                )
+    except ExtensionNotFound:
+        pass
+    else:
+        ids.extend([DNSPattern(name.encode('utf-8'))
+                    for name
+                    in ext.value.get_values_for_type(DNSName)])
+        ids.extend([URIPattern(uri.encode('utf-8'))
+                    for uri
+                    in ext.value.get_values_for_type(
+                        UniformResourceIdentifier)])
+        for other in ext.value.get_values_for_type(OtherName):
+            if other.type_id == ID_ON_DNS_SRV:
+                srv, _ = decode(other.value)
+                if isinstance(srv, IA5String):
+                    ids.append(SRVPattern(srv.asOctets()))
+                else:  # pragma: nocover
+                    raise CertificateError(
+                        "Unexpected certificate content."
+                    )
+
+    if not ids:
+        # http://tools.ietf.org/search/rfc6125#section-6.4.4
+        # A client MUST NOT seek a match for a reference identifier of CN-ID if
+        # the presented identifiers include a DNS-ID, SRV-ID, URI-ID, or any
+        # application-specific identifier types supported by the client.
+        cns = [n.value
+               for n
+               in cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)]
+        cn = next(iter(cns), b'<not given>')
+        ids = [DNSPattern(n.encode('utf-8')) for n in cns]
+        warnings.warn(
+            "Certificate with CN {!r} has no `subjectAltName`, falling back "
+            "to check for a `commonName` for now.  This feature is being "
+            "removed by major browsers and deprecated by RFC 2818.".format(cn),
+            SubjectAltNameWarning
+        )
+    return ids
