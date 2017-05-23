6292e32673329921221be1d672be9b28ff416235
Hynek Schlawack <hs@ox.cx>
2017-05-23 11:48:42 -0700

    Make SubjectAltNameWarning ad DeprecationWarning
    ...to uphold our deprecation policy.

--- src/service_identity/exceptions.py
+++ src/service_identity/exceptions.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import, division, print_function
 import attr
 
 
-class SubjectAltNameWarning(Warning):
+class SubjectAltNameWarning(DeprecationWarning):
     """
     Server Certificate does not contain a ``SubjectAltName``.
 
